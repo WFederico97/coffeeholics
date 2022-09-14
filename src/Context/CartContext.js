2e3cd78b8a2d2454f6c7558bc157faaf3a10ddc4
Federico W <wuthrich09@gmail.com>
2022-09-14 19:00:22 -0300

    ADD: cart context

---
+++ src/Context/CartContext.js
@@ -0,0 +1,66 @@
+import React, {  useContext, useState } from "react";
+
+const CartContext = React.createContext([]);
+
+export const useCartContext = () => useContext(CartContext)
+
+const CartProvider = ({ children }) => { 
+
+    const [cartList, setCartList] = useState([]);
+
+    function isInCart(id){
+        return cartList.some(item => item.id === id)
+    }
+
+    function addCart(producto, qty) {
+        if(qty === 0){
+            return;
+        }
+        let exist = isInCart(producto.id)
+
+        if(exist){
+            let itemCart = cartList.find(item => item.id === producto.id);
+            itemCart.qty += qty;
+            setCartList([...cartList]);
+        }else {
+            setCartList([...cartList, {...producto, qty}])
+        }
+    }
+
+    function qtyItems () {
+        return cartList.reduce((total, item) => total + item.qty, 0)
+    }
+
+    function removeItem(id) {
+        setCartList(cartList.filter(item=>  item.id !== id ))
+    }
+
+    function clearCart(){
+        setCartList([])
+    }
+
+    function montoTotalCart() {
+        return cartList.reduce((total, item)=> total + (item.qty * item.precio), 0)
+    }
+
+    function IVA() {
+        return montoTotalCart() * 0.21
+    }
+
+    function montoFinal(){
+        return montoTotalCart() + IVA()
+    }
+
+    return (
+        <CartContext.Provider value={
+            {
+                cartList, setCartList, qtyItems, clearCart,addCart, removeItem, montoTotalCart, IVA, montoFinal
+            }
+        }>
+            {children}
+
+        </CartContext.Provider>
+    )
+}
+export default CartProvider;
+
