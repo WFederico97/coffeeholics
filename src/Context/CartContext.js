172c6cf02e83c624590b81eb87b9ca691d7648fe
Federico W <wuthrich09@gmail.com>
2022-09-19 00:24:35 -0300

    ADD: localstorage

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,60 +1,69 @@
-import React, {  useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const CartContext = React.createContext([]);
 
 export const useCartContext = () => useContext(CartContext)
 
-const CartProvider = ({ children }) => { 
+const CartProvider = ({ children }) => {
+    let getCartFromLs = JSON.parse(window.localStorage.getItem('purchase'))
+    if (getCartFromLs == null) {
+        getCartFromLs = [];
+    }
+    
+    const [cartList, setCartList] = useState(getCartFromLs);
 
-    const [cartList, setCartList] = useState([]);
 
-    function isInCart(id){
+    function isInCart(id) {
         return cartList.some(item => item.id === id)
     }
 
     function addCart(producto, qty) {
-        if(qty === 0){
+        if (qty === 0) {
             return;
         }
         let exist = isInCart(producto.id)
 
-        if(exist){
+        if (exist) {
             let itemCart = cartList.find(item => item.id === producto.id);
             itemCart.qty += qty;
             setCartList([...cartList]);
-        }else {
-            setCartList([...cartList, {...producto, qty}])
+        } else {
+            setCartList([...cartList, { ...producto, qty }])
         }
     }
 
-    function qtyItems () {
+    function qtyItems() {
         return cartList.reduce((total, item) => total + item.qty, 0)
     }
 
     function removeItem(id) {
-        setCartList(cartList.filter(item=>  item.id !== id ))
+        setCartList(cartList.filter(item => item.id !== id))
     }
 
-    function clearCart(){
+    function clearCart() {
         setCartList([])
     }
 
     function montoTotalCart() {
-        return cartList.reduce((total, item)=> total + (item.qty * item.price), 0)
+        return cartList.reduce((total, item) => total + (item.qty * item.price), 0)
     }
 
     function IVA() {
         return montoTotalCart() * 0.21
     }
 
-    function montoFinal(){
+    function montoFinal() {
         return montoTotalCart() + IVA()
     }
+
+    useEffect(()=>{
+        window.localStorage.setItem('purchase', JSON.stringify(cartList))
+    }, [cartList])
 
     return (
         <CartContext.Provider value={
             {
-                cartList, setCartList, qtyItems, clearCart,addCart, removeItem, montoTotalCart, IVA, montoFinal
+                cartList, setCartList, qtyItems, clearCart, addCart, removeItem, montoTotalCart, IVA, montoFinal
             }
         }>
             {children}
