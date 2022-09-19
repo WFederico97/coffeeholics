1b58255dc6fdafb7169a2cd745d0aefda7a31fa5
Federico W <wuthrich09@gmail.com>
2022-09-19 00:25:26 -0300

    ADD: cartlist widget to cart

--- src/Components/Cart/Cart Widget/Cart-Widget.js
+++ src/Components/Cart/Cart Widget/Cart-Widget.js
@@ -1,12 +1,21 @@
+import React from "react";
 import { RiShoppingCart2Fill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import './Cart-Widget.scss'
 
+import { useCartContext } from '../../../Context/CartContext'
+
+
 const CartWidget = () => {
 
+    const { qtyItems } = useCartContext();
     return (
-        <Link to={'/cart'} >
+        <Link to={'/cart'} className='cartWidget-container-link'>
+            <div className="cartWidget-container">
             <RiShoppingCart2Fill id='cart' className='navbarIcon' style={{ width: '2em', height: '2em', color: '#fff', margin: '1em' }} />
+            <p className="navbarIcon-qtyItems">{qtyItems()}</p>
+            </div>
+
         </Link>
 
     )
