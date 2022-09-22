05d4522b36087c36af29811cf2bd09a31dfb83e6
Federico W <wuthrich09@gmail.com>
2022-09-21 23:01:34 -0300

    FIX: cart container

--- src/Components/Cart/CartContainer/CartContainer.js
+++ src/Components/Cart/CartContainer/CartContainer.js
@@ -5,9 +5,10 @@ import { useCartContext } from "../../../Context/CartContext";
 import CartDetail from "../CartDetail/CartDetail";
 import { updateStock } from "../../../Firebase/firebaseCalls";
 import Container from "react-bootstrap/esm/Container";
+import NoItemModal from "../../Modals/NoItemsModal/NoItemModal";
 
 export default function CartContainer() {
-    const {cartList, clearCart, qtyItems, montoTotalCart, IVA, montoFinal} = useCartContext();
+    const { cartList, clearCart, qtyItems, montoTotalCart, IVA, montoFinal } = useCartContext();
 
     const handleClearCarrito = () => {
         cartList.forEach(item => {
@@ -16,43 +17,61 @@ export default function CartContainer() {
         clearCart();
     }
 
-    return(
+    return (
         <>
-        <div className="cartContainer-title">
-            <h2>
-                Mi carrito ({qtyItems()} {qtyItems() > 1 ? 'items' : 'item'})
-            </h2>
-        </div>
-        <div className="cartContainer-keepShopping-btn">
-            <Link to={'/shop'}>
-                <h4>Seguir comprando</h4>
-            </Link>
-            <h4 onClick={handleClearCarrito} className='cartContainer-clearCart'>Vaciar Carrito</h4>
-        </div>
-
-        <Container className="cartDetail-container">
-            <div className="cartDetail-leftSide">
-                {cartList.map((item) => (
-                    <CartDetail key={item.id} item={item}  />
-                ))}
-            </div>
-            <div className="cartDetail-rightSide">
-                <h2>Tu pedido</h2>
-
-                <div className="cartDetail-rightSide-checkout">
-                    <h3> Subtotal ($): </h3>
-                    <p>{montoTotalCart()}</p>
+            <div className="cartContainer-title d-flex col-12">
+                <div className="cartContainer-title-qtyCart col-6">
+                    <h2>
+                        Mi carrito ({qtyItems()} {qtyItems() > 1 ? 'items' : 'item'})
+                    </h2>
                 </div>
-                <div className="cartDetail-rightSide-checkout">
-                    <h3> IVA - 21% ($): </h3>
-                    <p>{IVA()}</p>
-                </div>
-                <div className="cartDetail-rightSide-checkout">
-                    <h3> TOTAL ($): </h3>
-                    <p>{montoFinal()}</p>
+                <div className="cartContainer-title-keepshopingBtn d-flex col-6">
+                    <Link to={'/shop'} className='cartContainer-title-keepshopingBtn-link'>
+                        <h4 className="col-2">Seguir Comprando</h4>
+                    </Link>
+                    <h4 className="col-2" onClick={handleClearCarrito} >Vaciar Carrito</h4>
                 </div>
             </div>
-        </Container>
+            <Container className="cartDetail-container d-flex">
+                <div className="cartDetail-container-leftSide" >
+                    {
+                        cartList.map((item) => (<CartDetail key={item.id} item={item} />
+                        ))}
+                </div>
+                <div className="cartDetail-container-rightSide">
+                    <h3 className="cartDetail-container-rightSide-h3">Resumen pedido</h3>
+
+                    <div className="cartDetail-container-rightSide-section">
+                        <h4 className="cartDetail-container-rightSide-section-h4">Subtotal ($) : </h4>
+                        <p className="cartDetail-container-rightSide-section-p">{montoTotalCart()}</p>
+                        <hr />
+                    </div>
+                    <div className="cartDetail-container-rightSide-section" >
+                        <h4 className="cartDetail-container-rightSide-section-h4"> + IVA :</h4>
+                        <p className="cartDetail-container-rightSide-section-p">{IVA()}</p>
+                        <hr />
+                    </div>
+                    <div className="cartDetail-container-rightSide-section">
+                        <h4 className="cartDetail-container-rightSide-section-h4"> TOTAL :</h4>
+                        <p className="cartDetail-container-rightSide-section-p">{montoFinal()}</p>
+                        <hr />
+                    </div>
+
+                    <div className="cartDetail-container-righSide-section-logos ">
+                        <img src={"./Images/amex-svgrepo-com.svg"} alt='amex logo'/>
+                        <img src={"./Images/apple-pay-payment-mark-logo-svgrepo-com.svg"} alt='apple pay logo'/>
+                        <img src={"./Images/google-pay-primary-logo-logo-svgrepo-com.svg"} alt='google pay logo'/>
+                        <img src={"./Images/mastercard-svgrepo-com.svg"} alt='mastercard logo'/>
+                        <img src={"./Images/paypal-svgrepo-com.svg"} alt='paypal logo'/>
+                    </div>
+
+                    {/* <div className="buttons-container">
+                        {
+                            qtyItems > 0 ? <FormCompra /> : <NoItemModal />
+                        }
+                    </div> */}
+                </div>
+            </Container>
         </>
     )
 }
