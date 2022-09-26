d9baaeb37951c597f7470d39509574b5535c47bd
Federico W <wuthrich09@gmail.com>
2022-09-26 17:39:07 -0300

    FIX: Spinner

--- src/Utils/Spinner/Spinner.js
+++ src/Utils/Spinner/Spinner.js
@@ -3,9 +3,11 @@ import './Spinner.scss'
 
 export default function SpinnerLoading() {
     return (
-        <div className='loading-spinner-container'>
-            <Spinner className='loading-spinner'  />
-            <h3 className='loading-spinner-h3'>Cargando productos ...</h3>
+        <div className='loading-spinner-container d-flex col flex-wrap  col-12 '>
+            <Spinner animation="border" role="status" className='loading-spinner col-12 ' />
+            <h3 className='loading-spinner-h3 col-12 '>Cargando productos ...</h3>
+
+
         </div>
     )
 }
