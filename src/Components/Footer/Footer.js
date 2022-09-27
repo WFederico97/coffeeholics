be39ac14192f486f222a9ce947097d4ecde126f4
Federico W <wuthrich09@gmail.com>
2022-09-27 15:32:03 -0300

    FIX: sticky footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,13 +7,11 @@ const Footer = () => {
     return (
         <div className="fixed-bottom">
             <Navbar className="footer" >
-                <Container className="justify-content-center d-flex py-3 col-12 ">
+                <Container className="justify-content-center d-flex py-0 col-12 ">
                     <Navbar.Brand className="text-white">
                     Coded with <RiUserHeartLine className="m-1" /> by  <RiLinkedinBoxFill className="m-1 " /><a href="https://www.linkedin.com/in/federico-wuthrich-668633164/" target={'_blank'} rel="noreferrer" className="text-light">Federico</a>
-                    <a href="/" rel="noreferrer"><RiArrowUpLine className="backHome-icon m-1 text-light" /></a>
-                    
-                    </Navbar.Brand>
-                    
+                    <a href="/" rel="noreferrer"><RiArrowUpLine className="backHome-icon m-1 text-light" /></a>                   
+                    </Navbar.Brand>    
                 </Container>
             </Navbar>
         </div>
