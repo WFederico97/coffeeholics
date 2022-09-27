6150925fe63a2d3c50f7e184bdf04f9d83490637
Federico W <wuthrich09@gmail.com>
2022-09-27 13:19:52 -0300

    FIX: sticky footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,22 @@
-import { RiUserHeartLine, RiLinkedinBoxFill } from "react-icons/ri";
+import Container from "react-bootstrap/esm/Container";
+import Navbar from 'react-bootstrap/Navbar';
+import { RiUserHeartLine, RiLinkedinBoxFill, RiArrowUpLine } from "react-icons/ri";
+import './Footer.scss'
 
 const Footer = () => {
     return (
-        <footer className="mt-auto text-white-50 justify-content-center d-flex">
-            <p>Coded with <RiUserHeartLine /> by  <RiLinkedinBoxFill /><a href="https://www.linkedin.com/in/federico-wuthrich-668633164/" target={'_blank'} rel="noreferrer" className="text-white">Federico</a>.</p>
-        </footer>
+        <div className="fixed-bottom">
+            <Navbar className="footer" >
+                <Container className="justify-content-center d-flex py-3 col-12 ">
+                    <Navbar.Brand className="text-white">
+                    Coded with <RiUserHeartLine className="m-1" /> by  <RiLinkedinBoxFill className="m-1 " /><a href="https://www.linkedin.com/in/federico-wuthrich-668633164/" target={'_blank'} rel="noreferrer" className="text-light">Federico</a>
+                    <a href="/" rel="noreferrer"><RiArrowUpLine className="backHome-icon m-1 text-light" /></a>
+                    
+                    </Navbar.Brand>
+                    
+                </Container>
+            </Navbar>
+        </div>
     )
 }
 export default Footer
